8efd46c9fc47917cb3c60bc5ffb72b042618d2e3
Jonas Örnfelt <archornf@gmail.com>
2023-02-11 11:42:53 +0100

    Bots

--- src/server/game/Instances/InstanceScript.cpp
+++ src/server/game/Instances/InstanceScript.cpp
@@ -32,6 +32,10 @@
 #include "Spell.h"
 #include "WorldSession.h"
 
+//npcbot
+#include "botmgr.h"
+//end npcbot
+
 BossBoundaryData::~BossBoundaryData()
 {
     for (const_iterator it = begin(); it != end(); ++it)
@@ -497,6 +501,14 @@ void InstanceScript::DoRemoveAurasDueToSpellOnPlayers(uint32 spell)
                 player->RemoveAurasDueToSpell(spell);
                 if (Pet* pet = player->GetPet())
                     pet->RemoveAurasDueToSpell(spell);
+                //npcbot: include bots
+                if (player->HaveBot())
+                {
+                    for (auto const& bitr : *player->GetBotMgr()->GetBotMap())
+                        if (bitr.second && bitr.second->IsInWorld())
+                            DoRemoveAurasDueToSpellOnNPCBot(bitr.second, spell);
+                }
+                //end npcbot
             }
         }
     }
@@ -510,8 +522,36 @@ void InstanceScript::DoCastSpellOnPlayers(uint32 spell)
     if (!PlayerList.IsEmpty())
         for (Map::PlayerList::const_iterator i = PlayerList.begin(); i != PlayerList.end(); ++i)
             if (Player* player = i->GetSource())
+            {
                 player->CastSpell(player, spell, true);
+                //npcbot: include bots
+                if (player->HaveBot())
+                {
+                    for (auto const& bitr : *player->GetBotMgr()->GetBotMap())
+                        if (bitr.second && bitr.second->IsInWorld())
+                            DoCastSpellOnNPCBot(bitr.second, spell);
+                }
+                //end npcbot
+            }
 }
+
+//npcbot: hooks
+void InstanceScript::DoRemoveAurasDueToSpellOnNPCBot(Creature* bot, uint32 spell)
+{
+    ASSERT(bot && bot->IsNPCBot() && bot->IsInWorld() && !bot->IsFreeBot());
+    bot->RemoveAurasDueToSpell(spell);
+    if (Unit* botpet = bot->GetBotsPet())
+        botpet->RemoveAurasDueToSpell(spell);
+}
+
+void InstanceScript::DoCastSpellOnNPCBot(Creature* bot, uint32 spell)
+{
+    ASSERT(bot && bot->IsNPCBot() && bot->IsInWorld() && !bot->IsFreeBot());
+    bot->CastSpell(bot, spell, true);
+    if (Unit* botpet = bot->GetBotsPet())
+        botpet->CastSpell(botpet, spell, true);
+}
+//end npcbot
 
 bool InstanceScript::CheckAchievementCriteriaMeet(uint32 criteria_id, Player const* /*source*/, Unit const* /*target*/ /*= nullptr*/, uint32 /*miscvalue1*/ /*= 0*/)
 {
